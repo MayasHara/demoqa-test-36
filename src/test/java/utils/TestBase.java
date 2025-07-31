ec73c768b1be4a6f2b5d6ec9bb4cf5e1c515c0da
MayasHara <tataurov.vlad@gmail.com>
2025-07-31 23:04:59 +0900

    Added attach

--- src/test/java/utils/TestBase.java
+++ src/test/java/utils/TestBase.java
@@ -4,8 +4,12 @@ import com.codeborne.selenide.Configuration;
 import helpers.Attach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
+import java.util.Objects;
+
+import static java.util.List.of;
 
 public class TestBase {
 
@@ -17,6 +21,13 @@ public class TestBase {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
     }
     @AfterEach
     void addAttachments(){
