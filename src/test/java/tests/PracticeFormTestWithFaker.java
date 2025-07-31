5f916080f76e9929286559c2a8c2d11f72ab583c
MayasHara <tataurov.vlad@gmail.com>
2025-07-31 16:23:04 +0900

    Added allure

--- src/test/java/tests/PracticeFormTestWithFaker.java
+++ src/test/java/tests/PracticeFormTestWithFaker.java
@@ -1,10 +1,14 @@
 package tests;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 import utils.GeneratorUtils;
 import utils.TestBase;
 import utils.TestData;
+
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 
 public class PracticeFormTestWithFaker  extends TestBase {
@@ -15,30 +19,40 @@ public class PracticeFormTestWithFaker  extends TestBase {
 
     @Test
     void successfulRegistrationTest() {
-        registrationPage.openPage()
-                .removeAds()
-                .setFirstName(data.userName)
-                .setLastName(data.lastName)
-                .setEmail(data.userEmailAddress)
-                .setGender(data.genderOfUser)
-                .setUserNumber(data.number)
-                .setDateOfBirth(data.day, data.month, data.year)
-                .setObjectInput(data.subjectOfUsers)
-                .setHobbiesWrapper(data.hobbies)
-                .setPicture(generator.pictureName())
-                .setAdress(data.address)
-                .selectStateAndCity(data.state, data.city)
-                .sendData();
-
-        registrationPage.checkResult("Student Name", data.userName + " " + data.lastName)
-                .checkResult("Student Email", data.userEmailAddress)
-                .checkResult("Gender", data.genderOfUser)
-                .checkResult("Mobile", data.number)
-                .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
-                .checkResult("Subjects", data.subjectOfUsers)
-                .checkResult("Hobbies", data.hobbies)
-                .checkResult("Picture", generator.pictureName())
-                .checkResult("Address", data.address)
-                .checkResult("State and City", data.state + " " + data.city);
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем страницу",() -> {
+            registrationPage.openPage();
+        });
+        step("Удаляем рекламу", () -> {
+            registrationPage.removeAds();
+        });
+        step("Заполняем данные",() -> {
+            registrationPage.setFirstName(data.userName)
+                    .setLastName(data.lastName)
+                    .setEmail(data.userEmailAddress)
+                    .setGender(data.genderOfUser)
+                    .setUserNumber(data.number)
+                    .setDateOfBirth(data.day, data.month, data.year)
+                    .setObjectInput(data.subjectOfUsers)
+                    .setHobbiesWrapper(data.hobbies)
+                    .setPicture(generator.pictureName())
+                    .setAdress(data.address)
+                    .selectStateAndCity(data.state, data.city);
+        });
+        step("Отправляем данные", () -> {
+            registrationPage.sendData();
+        });
+        step("Проверка корректности заполненных данных", () -> {
+            registrationPage.checkResult("Student Name", data.userName + " " + data.lastName)
+                    .checkResult("Student Email", data.userEmailAddress)
+                    .checkResult("Gender", data.genderOfUser)
+                    .checkResult("Mobile", data.number)
+                    .checkResult("Date of Birth", data.day + " " + data.month + "," + data.year)
+                    .checkResult("Subjects", data.subjectOfUsers)
+                    .checkResult("Hobbies", data.hobbies)
+                    .checkResult("Picture", generator.pictureName())
+                    .checkResult("Address", data.address)
+                    .checkResult("State and City", data.state + " " + data.city);
+        });
     }
 }
